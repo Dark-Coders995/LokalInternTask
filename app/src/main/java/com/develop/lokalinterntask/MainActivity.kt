4366d2fd4a07195f2d8cbca277d126b3b9b59de4
Ayush Gupta <59391849+Dark-Coders995@users.noreply.github.com>
2025-04-24 16:21:51 +0530

    Task Started
    Initial Commit

---
+++ app/src/main/java/com/develop/lokalinterntask/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.develop.lokalinterntask
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.develop.lokalinterntask.ui.theme.LokalInternTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LokalInternTaskTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LokalInternTaskTheme {
+        Greeting("Android")
+    }
+}
